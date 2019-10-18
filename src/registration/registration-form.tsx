70bf4af5b45eaeef9a0a5a5aa9f4b74c91c1bbc1
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-10-18 22:56:21 +0200

    Styled

--- src/registration/registration-form.tsx
+++ src/registration/registration-form.tsx
@@ -11,60 +11,65 @@ const RegistrationForm = ({ person: initalPerson, onRegister }: any) => {
   };
 
   return (
-    <div>
-      <h2>Registration</h2>
-      <div>
-        <label htmlFor="firstName">Firstname</label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          value={person.firstName}
-          onChange={handleChange}
-        />
+    <div className="card" style={{ marginTop: 24 }}>
+      <div className="card-header">
+        <h2>Registration</h2>
       </div>
-      <div>
-        <label htmlFor="lastName">Lastname</label>
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          value={person.lastName}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="country">Country</label>
-        <select
-          id="country"
-          name="country"
-          value={person.country}
-          onChange={handleChange}
-        >
-          <option value="NL">Netherlands</option>
-          <option value="USA">USA</option>
-        </select>
-      </div>
-      {person.country === 'USA' && (
-        <div>
-          <label htmlFor="state">State</label>
+      <div className="card-body">
+        <div className="form-group">
+          <label htmlFor="firstName">Firstname</label>
           <input
             type="text"
-            id="state"
-            name="state"
-            value={person.state}
+            id="firstName"
+            name="firstName"
+            value={person.firstName}
             onChange={handleChange}
           />
         </div>
-      )}
+        <div className="form-group">
+          <label htmlFor="lastName">Lastname</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={person.lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="country">Country</label>
+          <select
+            id="country"
+            name="country"
+            value={person.country}
+            onChange={handleChange}
+          >
+            <option value="NL">Netherlands</option>
+            <option value="USA">USA</option>
+          </select>
+        </div>
+        {person.country === 'USA' && (
+          <div className="form-group">
+            <label htmlFor="state">State</label>
+            <input
+              type="text"
+              id="state"
+              name="state"
+              value={person.state}
+              onChange={handleChange}
+            />
+          </div>
+        )}
 
-      <div>
-        <button
-          disabled={!person.firstName || !person.lastName}
-          onClick={() => onRegister && onRegister(person)}
-        >
-          Register
-        </button>
+        <div className="float-right">
+          <button
+            disabled={!person.firstName || !person.lastName}
+            onClick={() => onRegister && onRegister(person)}
+            className="btn btn-primary"
+          >
+            Register
+          </button>
+        </div>
       </div>
     </div>
   );
