b7c7be835690bfc0dc0c76d0701a5e822a4158b2
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-10-19 10:59:29 +0200

    Pause on step

--- src/registration/registration-form.test.tsx
+++ src/registration/registration-form.test.tsx
@@ -6,76 +6,13 @@ import RegistrationForm from './registration-form';
 
 describe('The RegistrationForm', () => {
   test('render the empty registration form', () => {
-    const { getByText, getByLabelText, queryByLabelText } = render(
-      <RegistrationForm />
-    );
-
-    expect(getByText('Registration')).toBeVisible();
-    expect(getByText('Register')).toBeDisabled();
-    expect(getByLabelText('Firstname')).toBeVisible();
-    expect(getByLabelText('Lastname')).toBeVisible();
-    expect(getByLabelText('Country')).toBeVisible();
-    expect(queryByLabelText('State')).not.toBeInTheDocument();
-  });
-
-  test('render the populated registration form', () => {
-    const person = {
-      firstName: 'Maurice',
-      lastName: 'de Beijer',
-      country: 'NL',
-      state: ''
-    };
-    const { getByText, getByLabelText, queryByLabelText } = render(
-      <RegistrationForm person={person} />
-    );
-
-    expect(getByText('Registration')).toBeVisible();
-    expect(getByText('Register')).toBeEnabled();
-    expect(getByLabelText('Firstname')).toHaveValue('Maurice');
-    expect(getByLabelText('Lastname')).toHaveValue('de Beijer');
-    expect(getByLabelText('Country')).toHaveValue('NL');
-    expect(queryByLabelText('State')).not.toBeInTheDocument();
-  });
-
-  test('render the USA based populated registration form', () => {
-    const person = {
-      firstName: 'Chuck',
-      lastName: 'Norris',
-      country: 'USA',
-      state: 'Texas'
-    };
-    const { getByText, getByLabelText } = render(
-      <RegistrationForm person={person} />
-    );
+    const { getByText, getByLabelText } = render(<RegistrationForm />);
 
     expect(getByText('Registration')).toBeVisible();
     expect(getByText('Register')).toBeVisible();
-    expect(getByLabelText('Firstname')).toHaveValue('Chuck');
-    expect(getByLabelText('Lastname')).toHaveValue('Norris');
-    expect(getByLabelText('Country')).toHaveValue('USA');
-    expect(getByLabelText('State')).toHaveValue('Texas');
-  });
-
-  test('register a person', () => {
-    const onRegister = jest.fn();
-    const { getByText, getByLabelText } = render(
-      <RegistrationForm onRegister={onRegister} />
-    );
-
-    fireEvent.change(getByLabelText('Firstname'), {
-      target: { name: 'firstName', value: 'Mike' }
-    });
-    fireEvent.change(getByLabelText('Lastname'), {
-      target: { name: 'lastName', value: 'Jones' }
-    });
-
-    fireEvent.click(getByText('Register'));
-
-    expect(onRegister).toHaveBeenCalledWith({
-      firstName: 'Mike',
-      lastName: 'Jones',
-      country: 'NL',
-      state: ''
-    });
+    expect(getByLabelText('Firstname')).toBeVisible();
+    expect(getByLabelText('Lastname')).toBeVisible();
+    expect(getByLabelText('Country')).toBeVisible();
+    expect(getByLabelText('State')).toBeVisible();
   });
 });
--- src/registration/registration-form.tsx
+++ src/registration/registration-form.tsx
@@ -1,84 +1,33 @@
 import React from 'react';
 
-const RegistrationForm = ({ person: initalPerson, onRegister }: any) => {
-  const [person, setPerson] = React.useState<Person>(initalPerson);
-
-  const handleChange = ({
-    target
-  }: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value } = target;
-    setPerson((p: Person) => ({ ...p, [name]: value }));
-  };
-
+const RegistrationForm = () => {
   return (
-    <div className="card" style={{ marginTop: 24 }}>
-      <div className="card-header">
-        <h2>Registration</h2>
+    <div>
+      <h2>Registration</h2>
+      <div>
+        <label htmlFor="firstName">Firstname</label>
+        <input type="text" id="firstName" name="firstName" />
       </div>
-      <div className="card-body">
-        <div className="form-group">
-          <label htmlFor="firstName">Firstname</label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            value={person.firstName}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="lastName">Lastname</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            value={person.lastName}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="country">Country</label>
-          <select
-            id="country"
-            name="country"
-            value={person.country}
-            onChange={handleChange}
-          >
-            <option value="NL">Netherlands</option>
-            <option value="USA">USA</option>
-          </select>
-        </div>
-        {person.country === 'USA' && (
-          <div className="form-group">
-            <label htmlFor="state">State</label>
-            <input
-              type="text"
-              id="state"
-              name="state"
-              value={person.state}
-              onChange={handleChange}
-            />
-          </div>
-        )}
-
-        <div className="float-right">
-          <button
-            disabled={!person.firstName || !person.lastName}
-            onClick={() => onRegister && onRegister(person)}
-            className="btn btn-primary"
-          >
-            Register
-          </button>
-        </div>
+      <div>
+        <label htmlFor="lastName">Lastname</label>
+        <input type="text" id="lastName" name="lastName" />
+      </div>
+      <div>
+        <label htmlFor="country">Country</label>
+        <select id="country" name="country">
+          <option value="NL">Netherlands</option>
+          <option value="USA">USA</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="state">State</label>
+        <input type="text" id="state" name="state" />
+      </div>
+      <div>
+        <button>Register</button>
       </div>
     </div>
   );
 };
-
-RegistrationForm.defaultProps = {
-  person: { firstName: '', lastName: '', country: 'NL', state: '' }
-};
-
-type Person = typeof RegistrationForm.defaultProps.person;
 
 export default RegistrationForm;
